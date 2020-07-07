72f233ee8dd0261c037ea57dab7781de5c1120e9
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-07-07 13:41:46 -0700

    pass fourth test for index-reducer

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -23,6 +23,27 @@ describe('indexReducer', () => {
   test('initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, {type: null}))
   })
+  test("Check that initial state of postListReducer matches root reducer", () => {
+    const timestamp = Date.now();
+    const formattedTime =  new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(timestamp);
+     const action = {
+       type: 'ADD_POST',
+       username:'jhvozdovich',
+       post: 'I love cats',
+       upvotes: 0, 
+       downvotes: 0,
+       timestamp: formattedTime,
+       id:1
+     }
+     store.dispatch(action);
+     expect(store.getState().masterPostList).toEqual(postListReducer(undefined,action));
+  });
   
-  
+  test('check that TOGGLE_FORM displatch changes occur in formVisibleReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  })
 });

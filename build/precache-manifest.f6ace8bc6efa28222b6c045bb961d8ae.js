7a1e9cfed0ec331cee1cb88b3877ee2ae4dce464
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-07-09 16:53:06 -0700

    fix .gitignore for deployment

---
+++ build/precache-manifest.f6ace8bc6efa28222b6c045bb961d8ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd46aa2aec89142857f72d0d6afa05d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dc0d43cefda0050553e",
+    "url": "/static/css/main.19b924da.chunk.css"
+  },
+  {
+    "revision": "4db51fe18816a273dd5f",
+    "url": "/static/js/2.d1ea29c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d1ea29c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc0d43cefda0050553e",
+    "url": "/static/js/main.060a7d7a.chunk.js"
+  },
+  {
+    "revision": "c95772a33aee0b1ff509",
+    "url": "/static/js/runtime-main.1e72d196.js"
+  }
+]);
